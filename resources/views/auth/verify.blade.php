c2c1b36883ce09520da0df64672ab84e618204a3
imriel <imashi@protonmail.com>
2020-04-08 18:48:47 +0200

    Revert "Modification and translation of authentication views"
    This reverts commit 68fe0e21939320af022315b6e2ceb7a68cfbbb4e.
    
    cancelling translation, to be replaced with fr.json

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Vérification de l\'adresse e-mail') }}</div>
+                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('Un nouveau lien de vérification vient d\'être envoyé à votre adresse e-mail.') }}
+                            {{ __('A fresh verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
-                    {{ __('Avant de continuer, merci de vérifier votre boîte mail.') }}
-                    {{ __('Si vous n\'avez pas reçu l'e-mail') }},
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('cliquez ici pour en demander un nouveau') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
