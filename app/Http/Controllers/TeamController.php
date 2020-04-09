56f38bbceeaa71c9ea12ca54233addadd3b9fdf2
Axel Lheureux <axel.lheureux@gmail.com>
2020-04-09 19:06:02 +0200

    Test validator

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Position;
+use Illuminate\Support\Facades\Validator;
 
 class TeamController extends Controller
 {
@@ -16,9 +17,19 @@ class TeamController extends Controller
       return view('newteamform');
   }
 
-  public function createTeam($request)
+  public function createTeam(Request $request)
   {
+    $this->validator($request->all())->validate();
+  }
 
+  protected function validator(array $data)
+  {
+    'firstname' => ['required', 'string', 'max:255'],
+      return Validator::make($data, [
+          'name' => ['required', 'string', 'max:255'],
+          'description' => ['required', 'string', 'max:4096'],
+          'type' => ['required'],
+      ]);
   }
 }
  ?>
