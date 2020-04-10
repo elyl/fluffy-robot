a678c4acd9764e2bab5e50f4e2ce338b12a877d3
imriel <imashi@protonmail.com>
2020-04-10 15:55:31 +0200

    Correcting registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,9 +61,6 @@
                             </div>
                         </div>
 
-			<div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
