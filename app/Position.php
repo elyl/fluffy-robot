b9c19f85db6a320ce7648f402d45b6334d33fd10
Axel Lheureux <axel.lheureux@gmail.com>
2020-04-10 23:03:10 +0200

    Updating model

--- app/Position.php
+++ app/Position.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends model
 {
   public $timestamps = false;
+
+  protected $fillable = [
+    'name', 'descrption', 'type', 'resp'
+  ];
 }
  ?>
