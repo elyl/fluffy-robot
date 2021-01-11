41a15d94d786d2a3e7752b4a0b7e9430a866c2c6
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-11 17:57:26 +0100

    Wrong field name.

--- resources/views/teamlisttest.blade.php
+++ resources/views/teamlisttest.blade.php
@@ -3,7 +3,7 @@
   <h1> Liste des équipes</h1>
   <ul>
     @foreach ($teams as $key)
-    <li> {{$key->name}}  - {{$key->responsable()->name}} - <a href="">Voir</a></li> <!-- Oui y'a pas de lien, je l'ajouterai plus tard -->
+    <li> {{$key->name}}  - {{$key->responsable()->firstname}} - <a href="">Voir</a></li> <!-- Oui y'a pas de lien, je l'ajouterai plus tard -->
     @endforeach
   </ul>
 </body>
