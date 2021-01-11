ae4387e3642bccf49bb8894e54db16d9de602361
Elias <enki@faloran.fr>
2021-01-11 17:06:30 +0100

    Add header and footer views

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+		<div class="footer">
+			<hr width="75%" align=center>
+			<p> Made by elyl & imriel - CC0 - 2020</p>
+		</div>
+	</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Trolls&Légendes</title>
+
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+   </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+	        <div class="top-right links">
+        		<a href="http://vps809923.ovh.net/fluffy_robot/public/">Log out</a>
+                        <a href="http://vps809923.ovh.net/fluffy_robot/public/profile">Profile</a>
+			<a href="http://vps809923.ovh.net/fluffy_robot/public/application">Application</a>
+                </div>
+            
+            <div class="header">
+                <div class="title m-b-md">
+		    <a href="http://vps809923.ovh.net/fluffy_robot/public/"><img src="{{ asset('images/logo.png') }}" alt="Logo Trolls & Légendes" style="width:511px;height:175px;" /></a>
+                </div>
+
+                <div class="links header">
+                    <a href="http://vps809923.ovh.net/fluffy_robot/public/planning">Planning</a>
+                    <a href="http://vps809923.ovh.net/fluffy_robot/public/teams">Teams</a>
+               </div>
+            </div>
+        </div>
+
+
