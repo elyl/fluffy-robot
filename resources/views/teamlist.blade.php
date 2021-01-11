d84fe098fac90b25fdbaba37ab1b58eef692c221
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-11 17:27:53 +0100

    Added some informations for teamList

--- resources/views/teamlist.blade.php
+++ resources/views/teamlist.blade.php
@@ -2,8 +2,8 @@
 <body>
   <h1> Liste des équipes</h1>
   <ul>
-    @foreach ($teams as $key):
-    <li> {{$key->name}} </li>
+    @foreach ($teams as $key)
+    <li> {{$key->name}}  - {{$key->resp}} - <a>Voir</a></li> <!-- Oui y'a pas de lien, je l'ajouterai plus tard -->
     @endforeach
   </ul>
 </body>
