71705ac6af57b5b1c432496013b32a111e01078b
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-12 01:00:08 +0100

    Route error.

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Auth::routes();
 //Route::middleware(['Auth'])->group(function () {
   Route::get('/teamlist', 'TeamController@teamList');
   Route::get('/newTeam', 'TeamController@showNewTeamForm');
-  Route::post('/teamProfile/team/{id}', 'TeamController@showTeamProfile');
+  Route::get('/teamProfile/team/{id}', 'TeamController@showTeamProfile');
   Route::get('newShift/team/{id}', 'ShiftController@showNewTeamForm');
   Route::post('newShift', 'ShiftController@createShift');
   Route::get('/shiftsList', 'ShiftController@shiftsList');
