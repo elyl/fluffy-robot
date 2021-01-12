70baf5e4178ec8784432984f9169fcc2db2327ce
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-12 01:09:18 +0100

    I'm doing everything wrong.

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -22,7 +22,7 @@ class TeamController extends Controller
 
   public function showTeamProfile(Request $request)
   {
-    $team = Position::findOrFail($request('id'));
+    $team = Position::findOrFail($request->input('id'));
     return view('teamprofile', ['team' => $team]);
   }
 
