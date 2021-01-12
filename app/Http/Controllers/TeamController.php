f18ccc14082cec61b1bfd1697e835e8d8715adf8
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-12 01:05:50 +0100

    .

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -22,7 +22,7 @@ class TeamController extends Controller
 
   public function showTeamProfile(Request $request)
   {
-    $team = Position::find($request->input('id'));
+    $team = Position::findOrFail($request->('id'));
     return view('teamprofile', ['team' => $team]);
   }
 
