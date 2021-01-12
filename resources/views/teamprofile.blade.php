f72242c3eddcbf1198e30894a978d08b37747145
Axel Lheureux <axel.lheureux@gmail.com>
2021-01-12 01:39:11 +0100

    Typo

--- resources/views/teamprofile.blade.php
+++ resources/views/teamprofile.blade.php
@@ -1,8 +1,8 @@
 @include('header');
 
 <h1> Apperçu de l'équipe </h1>
-<h2><strong>Description</strong> {{$team->name}} </h2>
-<p> {{$team->description}} <br/><br/>
+<h2>{{$team->name}} </h2>
+<p><strong>Description : </strong>  {{$team->description}} <br/><br/>
 Chef équipe : <a href="">{{$team->responsable->firstname}} </a> <br/>
 Type équipe : NYI </p>
 
